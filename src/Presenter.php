5a2bf378fadf067673e923e8ece263b871954cf1
Anael Favre <a.favre@web-6.fr>
2015-11-09 18:22:43 +0100

    Translating default items to english

--- src/Presenter.php
+++ src/Presenter.php
@@ -33,15 +33,14 @@ class Presenter extends \Laracasts\Presenter\Presenter
 	public function actions()
 	{
 		return '
-			<a href="' . url($this->entity->getEndpoint() . '/' . $this->entity->id) . '" data-toggle="tooltip" title="Ouvrir la fiche"><span class="badge bg-light-blue"><i class="fa fa-search"></i></span></a>
-			<a href="' . url($this->entity->getEndpoint() . '/' . $this->entity->id) . '/edit" data-toggle="tooltip" title="Modifier la fiche"><span class="badge bg-yellow"><i class="fa fa-pencil"></i></span></a>
+			<a href="' . url($this->entity->getEndpoint() . '/' . $this->entity->id) . '" data-toggle="tooltip" title="See details"><span class="badge bg-light-blue"><i class="fa fa-search"></i></span></a>
+			<a href="' . url($this->entity->getEndpoint() . '/' . $this->entity->id) . '/edit" data-toggle="tooltip" title="Edit"><span class="badge bg-yellow"><i class="fa fa-pencil"></i></span></a>
 			<a href="' . url($this->entity->getEndpoint() . '/' . $this->entity->id) . '/edit"
 				data-method="delete"
 				data-toggle="confirmation"
-				data-confirm-content="Souhaitez-vous supprimer cet élément ?"
 				data-confirm-placement="left"
 				data-toggle="tooltip"
-				title="Supprimer">
+				title="Delete">
 				<span class="badge bg-red"><i class="fa fa-times"></i></span>
 			</a>
 		';
